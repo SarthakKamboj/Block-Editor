6d8b59e79b731284652943ef19ab595a78ddc2b3
SarthakKamboj <sarthakkamboj22@gmail.com>
2022-04-27 21:34:50 -0500

    rendering rectangle w/ ebo

--- main.cpp
+++ main.cpp
@@ -21,9 +21,15 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "}\0";
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-	 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f
+	 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+	-0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f   // top left 
+};
+
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
 };
 
 int main(int argc, char* args[]) {
@@ -62,10 +68,14 @@ int main(int argc, char* args[]) {
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
 
-	GLuint vbo;
+	GLuint vbo, ebo;
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
 
 	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
@@ -79,6 +89,7 @@ int main(int argc, char* args[]) {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -126,7 +137,7 @@ int main(int argc, char* args[]) {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glBindVertexArray(0);
 
